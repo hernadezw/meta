20f2ceb1a55d08428a42e5caee588d4cf0c41528
meta <lhernandez1203@gmail.com>
2023-01-05 11:28:01 -0600

    ventana admin add

--- cliente/admin.py
+++ cliente/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cliente, TipoCliente, Departamento, Municipio, Direccion
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(TipoCliente)
+admin.site.register(Departamento)
+admin.site.register(Municipio)
+admin.site.register(Direccion)  
